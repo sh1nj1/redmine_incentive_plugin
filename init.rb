53518228cccc0ebd039e134bb334808ea984f040
Soonoh Jung <soonoh.jung@gmail.com>
2018-03-28 01:21:36 +0900

    Change auther and .gitignore

--- init.rb
+++ init.rb
@@ -1,6 +1,6 @@
 Redmine::Plugin.register :redmine_incentive_plugin do
   name 'Redmine Incentive Plugin plugin'
-  author 'Author name'
+  author 'Soonoh Jung'
   description 'This is a plugin for Redmine'
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
