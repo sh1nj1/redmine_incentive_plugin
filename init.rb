146ed67842215a335660ce95292d8a2bdcade53b
Soonoh <soonoh.jung@gmail.com>
2024-12-19 17:20:52 +0900

    feat: support redmine 6.x
    test on 6.0.1.stable

--- init.rb
+++ init.rb
@@ -1,6 +1,10 @@
 require 'redmine'
-require 'redmine_incentive_plugin/user_patch'
-require 'redmine_incentive_plugin/hooks'
+
+if (Rails.configuration.respond_to?(:autoloader) && Rails.configuration.autoloader == :zeitwerk) || Rails.version > '7.0'
+  Rails.autoloaders.each { |loader| loader.ignore(File.dirname(__FILE__) + '/lib') }
+end
+require File.dirname(__FILE__) + '/lib/redmine_incentive_plugin/user_patch'
+require File.dirname(__FILE__) + '/lib/redmine_incentive_plugin/hooks'
 
 Redmine::Plugin.register :redmine_incentive_plugin do
   name 'Redmine Incentive Plugin'
@@ -13,9 +17,9 @@ Redmine::Plugin.register :redmine_incentive_plugin do
 
   settings :partial => 'redmine_incentive_plugin/settings',
            :default => {
-               :display_xp_in_user_format => true,
-               :custom_field_id__xp => ' ',
-               :custom_field_id__sp => ' ',
-               :user_point_expression => RedmineIncentivePlugin::DEFAULT_USER_POINT_EXPRESSION
+             :display_xp_in_user_format => true,
+             :custom_field_id__xp => ' ',
+             :custom_field_id__sp => ' ',
+             :user_point_expression => RedmineIncentivePlugin::DEFAULT_USER_POINT_EXPRESSION
            }
 end
